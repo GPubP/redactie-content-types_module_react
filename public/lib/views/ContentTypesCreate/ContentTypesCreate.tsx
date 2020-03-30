32340d24641ece92eea498d2f7c5e90f6dc56839
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-03-30 15:51:19 +0200

    feat(content-types-create-tabs): navigate with react router link

--- public/lib/views/ContentTypesCreate/ContentTypesCreate.tsx
+++ public/lib/views/ContentTypesCreate/ContentTypesCreate.tsx
@@ -1,6 +1,7 @@
 import { ContextHeader, ContextHeaderTopSection } from '@acpaas-ui/react-editorial-components';
 import Core, { ModuleRouteConfig, useBreadcrumbs } from '@redactie/redactie-core';
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import { BREADCRUMB_OPTIONS } from '../../content-types.const';
 import { ContentTypesRouteProps } from '../../content-types.types';
@@ -10,8 +11,15 @@ import { Tab } from '../../types';
 const ContentTypesCreate: FC<ContentTypesRouteProps> = ({ basePath, routes }) => {
 	const route = routes.find(item => item.path === `${basePath}/aanmaken`) || null;
 	const TABS: Tab[] = [
-		{ name: 'Instellingen', target: `${route?.path}/instellingen`, active: true },
-		{ name: 'Content Componenten', target: `${route?.path}/componenten` },
+		{
+			name: 'Instellingen',
+			target: `${route?.path}/instellingen`,
+			active: true,
+		},
+		{
+			name: 'Content Componenten',
+			target: `${route?.path}/componenten`,
+		},
 		{ name: 'Sites', target: `${route?.path}/sites` },
 	];
 
@@ -26,7 +34,11 @@ const ContentTypesCreate: FC<ContentTypesRouteProps> = ({ basePath, routes }) =>
 	 */
 	return (
 		<>
-			<ContextHeader tabs={TABS} title="Content type aanmaken">
+			<ContextHeader
+				tabs={TABS}
+				linkProps={(props: any) => ({ ...props, to: props.href, component: Link })}
+				title="Content type aanmaken"
+			>
 				<ContextHeaderTopSection>{breadcrumbs}</ContextHeaderTopSection>
 			</ContextHeader>
 			{Core.routes.render(route?.routes as ModuleRouteConfig[], {
