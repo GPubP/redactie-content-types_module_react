d2d771d58616c41dc66c3bf986a448be9d95d0e3
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-03-30 20:11:38 +0200

    fix(contentType): fix types

--- public/lib/content-types.types.ts
+++ public/lib/content-types.types.ts
@@ -5,18 +5,49 @@ export interface ContentTypesRouteProps {
 	routes: ModuleRouteConfig[];
 }
 
+export interface ContentTypeFieldSchema {
+	uuid?: string;
+	label: string;
+	module: string;
+	name: string;
+	dataType: string;
+	fieldType: string;
+	config: any;
+	validators: string[];
+	operators: Array<{
+		label: string;
+		value: string;
+	}>;
+	generalConfig: {
+		multiLanguage?: boolean;
+		required?: boolean;
+		hidden?: boolean;
+		min?: number;
+		max?: number;
+	};
+}
+
+export interface ContenTypeMetaSchema {
+	label: string;
+	safeLabel: string;
+	description: string;
+	created: string;
+	lastModified: string;
+	taxonomy: {
+		available: string[];
+		fieldType: string;
+		tags: string[];
+	};
+	deleted: false;
+	status?: string;
+	lastEditor: string;
+}
+
 export interface ContentTypeSchema {
+	_id: string;
 	uuid: string;
-	data: {
-		name: string;
-		description: string;
-		status: string;
-	};
-	meta: {
-		tenant: string;
-		createdAt: string;
-		updatedAt: string;
-	};
+	fields: ContentTypeFieldSchema[];
+	meta: ContenTypeMetaSchema;
 }
 
 export interface ContentTypesSchema {
