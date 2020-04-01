a699e6ac4955485bbe3a5a9fa011d30e19c875ca
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-04-01 10:59:47 +0200

    feat(red-147-form-validatie): edited settings form validation schema

--- public/lib/views/ContentTypeDetailSettings/ContentTypeDetailSettings.const.ts
+++ public/lib/views/ContentTypeDetailSettings/ContentTypeDetailSettings.const.ts
@@ -1,6 +1,8 @@
 import { object, string } from 'yup';
 
 export const CT_SETTINGS_VALIDATION_SCHEMA = object().shape({
-	name: string().required(),
-	description: string().required(),
+	meta: object().shape({
+		label: string().required(),
+		description: string().required(),
+	}),
 });
