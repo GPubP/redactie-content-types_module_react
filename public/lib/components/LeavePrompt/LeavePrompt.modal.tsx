917ebbaed84dee13bcc53ea4d98b9fe042aa618b
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-09-23 17:39:52 +0200

    feat(ct-detail-settings): add LeavePrompt

--- public/lib/components/LeavePrompt/LeavePrompt.modal.tsx
+++ public/lib/components/LeavePrompt/LeavePrompt.modal.tsx
@@ -16,20 +16,22 @@ const LeavePromptModal: FC<LeavePromptModalProps> = ({
 	return createPortal(
 		<div className={classnames('m-overlay', { 'is-active': show })}>
 			<div className="m-modal m-modal--large">
-				<div className="m-modal__header u-margin-bottom-xs">
-					<h4>{title}</h4>
-					{onClose && (
-						<Button
-							className="m-modal__close"
-							icon="times"
-							type="default"
-							transparent
-							onClick={onClose}
-						/>
-					)}
+				<div className="m-modal__content">
+					<div className="m-modal__header u-margin-bottom-xs">
+						<h4>{title}</h4>
+						{onClose && (
+							<Button
+								className="m-modal__close"
+								icon="times"
+								type="default"
+								transparent
+								onClick={onClose}
+							/>
+						)}
+					</div>
+					<div className="u-margin-bottom">{body}</div>
+					<div className="m-modal__footer row end-xs">{children}</div>
 				</div>
-				<div className="u-margin-bottom">{body}</div>
-				<div className="m-modal__footer row end-xs">{children}</div>
 			</div>
 		</div>,
 		node || document.body
