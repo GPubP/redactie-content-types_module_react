24704c11efcfde36fbbdccf4116ced09fe0e8660
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-10-16 11:04:30 +0200

    feat(parseFields): expose parseFields method via the API

--- public/lib/api/api.types.ts
+++ public/lib/api/api.types.ts
@@ -1,10 +1,14 @@
+import { parseFields } from '../helpers/parseFields/parseFields';
 import { ExternalTabOptions } from '../store/api/externalTabs';
+
+import { registerCTDetailTab } from './registerCTDetailTab';
 
 export { ContentTypeDetailModel, ContentTypeFieldDetailModel } from '../store/contentTypes';
 
 export { ExternalTabProps } from '../views/tenant/ContentTypesDetailExternal';
 
 export interface ContentTypeAPI {
-	registerCTDetailTab: (name: string, options: ExternalTabOptions) => void;
+	registerCTDetailTab: typeof registerCTDetailTab;
+	parseFields: typeof parseFields;
 }
 export { ExternalTabOptions };
--- public/lib/api/index.ts
+++ public/lib/api/index.ts
@@ -1,8 +1,11 @@
 import Core from '@redactie/redactie-core';
 
+import { parseFields } from '../helpers/parseFields/parseFields';
+
 import { registerCTDetailTab } from './registerCTDetailTab';
 
 export const registerContentTypeAPI = (): void =>
 	Core.modules.exposeModuleApi('content-type-module', {
 		registerCTDetailTab,
+		parseFields,
 	});
