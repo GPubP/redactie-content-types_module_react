f3c1ea03ecccf838a010e7fec6fe9843f28b46e5
Lotje <lotte-moors@hotmail.com>
2022-03-23 09:25:55 +0100

    feat: add option to translate content

--- public/lib/components/forms/FormCCSettings/FormCCSettings.tsx
+++ public/lib/components/forms/FormCCSettings/FormCCSettings.tsx
@@ -1,4 +1,11 @@
-import { Checkbox, RadioGroup, Textarea, TextField } from '@acpaas-ui/react-components';
+import {
+	Checkbox,
+	RadioGroup,
+	Textarea,
+	TextField,
+	Card,
+	CardBody,
+} from '@acpaas-ui/react-components';
 import { CopyValue, FormikOnChangeHandler } from '@redactie/utils';
 import { ErrorMessage, Field, Formik } from 'formik';
 import kebabCase from 'lodash.kebabcase';
@@ -20,6 +27,9 @@ const FormCCSettings: FC<FormCCSettingsProps> = ({
 	formikRef,
 }) => {
 	const [isMultiple, setIsMultiple] = useState((initialValues?.generalConfig?.max || 0) > 1);
+	const [isTranslatable, setIsTranslatable] = useState<boolean>(
+		(fieldTypeData.generalConfig.isTranslatable) || false
+	);
 	const [t] = useCoreTranslation();
 
 	const onFormSubmit = (values: CCSettingsFormState): void => {
@@ -43,6 +53,10 @@ const FormCCSettings: FC<FormCCSettingsProps> = ({
 				max: isMultiple ? values.generalConfig.max : 1,
 			},
 		});
+	};
+
+	const translatable = (): void => {
+		setIsTranslatable(!isTranslatable);
 	};
 
 	return (
@@ -175,6 +189,30 @@ const FormCCSettings: FC<FormCCSettingsProps> = ({
 								/>
 							</div>
 						)}
+						<div className="u-margin-top u-margin-bottom">
+						<Card>
+							<CardBody>
+								<h6>Vertalen</h6>
+								<p className="u-margin-top u-margin-bottom">
+									{fieldTypeData.generalConfig.isTranslatable
+										? 'Bepaal of er voor deze component andere inhoud kan ingegeven worden per taal. Zoniet blijft de inhoud over de talen heen gelijk.'
+										: 'Voor deze component kan er geen andere inhoud ingegeven worden per taal. De inhoud blijft over de talen heen gelijk.'}
+								</p>
+
+									<Field
+										checked={isTranslatable}
+										disabled={!fieldTypeData.generalConfig.isTranslatable}
+										onChange={translatable}
+										id="fieldTypeData.generalConfig.isTranslatable"
+										name="fieldTypeData.generalConfig.isTranslatable"
+										label="Inhoud vertalen"
+									/>
+
+
+							</CardBody>
+						</Card>
+						</div>
+
 					</>
 				);
 			}}
